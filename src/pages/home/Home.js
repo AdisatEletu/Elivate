1e92a54849776ade871fef27d6ebafc7d9cd9506
EmmyM_ighty <remi.emma04@gmail.com>
2021-06-06 14:25:23 +0100

    Implemented dynamic raffle updates

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,136 +1,156 @@
-import React, {useEffect, useState} from 'react';
-import {Banner} from "./components/Banner";
-import {HowItWorks} from "./components/HowItWorks";
-import {Filter} from "./components/Filter";
-import {RaffleTimer} from "./components/RaffleTimer";
-import {WatchlistBtn} from "./components/WatchlistBtn";
-import {RaffleCard} from "./components/RaffleCards";
-import {Ticket} from "./components/Ticket";
-import {HomepageWinners} from "./components/Winners";
-import {FormButton} from "../../components/forms/Button";
-import {Testimonials} from "./components/Testimonials";
+import React, { useEffect, useState } from "react";
+import { Banner } from "./components/Banner";
+import { HowItWorks } from "./components/HowItWorks";
+import { Filter } from "./components/Filter";
+import { FeaturedRaffleTitle } from "./components/FeaturedRaffleTitle";
+import { RaffleTimer } from "./components/RaffleTimer";
+import { WatchlistBtn } from "./components/WatchlistBtn";
+import { RaffleCard } from "./components/RaffleCards";
+import { Ticket } from "./components/Ticket";
+import { HomepageWinners } from "./components/Winners";
+import { FormButton } from "../../components/forms/Button";
+import { Testimonials } from "./components/Testimonials";
+
+// import { raffleFetcher } from "../../utils";
+import { getRequest } from "../../helper/request";
 
 const Home = () => {
-  
-  const [data, setData] = useState([{
-    image: 'https://image.similarpng.com/very-thumbnail/2020/04/cosmetic-products-ad-3d-skin-care-brand-cream-lotion-png.png',
-    subtitle: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-    title: "Pixel 4 modern edition",
-    isManual: true
-  },
+  const [data, setData] = useState([
     {
-      image: 'https://image.similarpng.com/very-thumbnail/2020/04/cosmetic-products-ad-3d-skin-care-brand-cream-lotion-png.png',
-      subtitle: "What do you say changed again the industry's standard dummy text ever since the 1500s.",
-      title: "Pixel 5 modern edition",
-      isManual: false
+      image:
+        "https://image.similarpng.com/very-thumbnail/2020/04/cosmetic-products-ad-3d-skin-care-brand-cream-lotion-png.png",
+      subtitle:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+      title: "Pixel 4 modern edition",
+      isManual: true,
     },
     {
-      image: 'https://image.similarpng.com/very-thumbnail/2020/04/cosmetic-products-ad-3d-skin-care-brand-cream-lotion-png.png',
-      subtitle: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+      image:
+        "https://image.similarpng.com/very-thumbnail/2020/04/cosmetic-products-ad-3d-skin-care-brand-cream-lotion-png.png",
+      subtitle:
+        "What do you say changed again the industry's standard dummy text ever since the 1500s.",
+      title: "Pixel 5 modern edition",
+      isManual: false,
+    },
+    {
+      image:
+        "https://image.similarpng.com/very-thumbnail/2020/04/cosmetic-products-ad-3d-skin-care-brand-cream-lotion-png.png",
+      subtitle:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
       title: "Pixel 6 modern edition",
-      isManual: true
-    }
+      isManual: true,
+    },
   ]);
-  
+
+  const [raffles, setRaffles] = useState([]);
+  const getRaffles = async () => {
+    try {
+      const { data, success } = await getRequest("customer/raffle");
+
+      if (success) {
+        setRaffles(data.data);
+      }
+    } catch (e) {}
+  };
+
   useEffect(() => {
-  
-  });
-  
-  
+    getRaffles();
+  }, []);
+
+  console.log(raffles);
+
   return (
     <div className="">
-      <Banner data={data}/>
-      <HowItWorks/>
-      <Filter classNames={'mt-6'}/>
-      
-      <div className={'mt-3'}>
-        <div className={"homepage-featured "}
-             style={{backgroundImage: `url(${require('../../assets/Elivate9ja/ladies.png')})`}}>
-          <div className={'homepage-cover col-md-12 m-flex m-pl-6'} style={{minHeight: " 325px"}}>
-            <div className={'col-md-4 ticket-details-holder m-show'}>
-              <Ticket ticket={5}/>
+      <Banner data={data} />
+      <HowItWorks />
+      <FeaturedRaffleTitle />
+      <Filter classNames={"mt-6"} />
+
+      <div className={"mt-3"}>
+        <div
+          className={"homepage-featured "}
+          style={{
+            backgroundImage: `url(${require("../../assets/Elivate9ja/ladies.png")})`,
+          }}
+        >
+          <div
+            className={"homepage-cover col-md-12 m-flex m-pl-6"}
+            style={{ minHeight: " 325px" }}
+          >
+            <div className={"col-md-4 ticket-details-holder m-show"}>
+              <Ticket ticket={5} />
             </div>
-            <div className={'col-md-4  m-flex flex-column m-p-2 justify-content-center'}>
-              <div className={'title1 white-color'}> iPhone SE</div>
-              <div className={'paragraph off-white-color'}>
-                The all new Iphone SE is up for grabsThe all new Iphone SE is up for grabs
+            <div
+              className={
+                "col-md-4  m-flex flex-column m-p-2 justify-content-center"
+              }
+            >
+              <div className={"title1 white-color"}> iPhone SE</div>
+              <div className={"paragraph off-white-color"}>
+                The all new Iphone SE is up for grabsThe all new Iphone SE is up
+                for grabs
               </div>
-              <div className={'col-md-10 mt-5'}>
-                <RaffleTimer timer={'2 hr : 30 mins : 27 sec'}/>
+              <div className={"col-md-10 mt-5"}>
+                <RaffleTimer timer={"2 hr : 30 mins : 27 sec"} />
               </div>
             </div>
-            <div className={'col-md-4 d-flex align-items-center m-p-2 m-justify-content-normal m-mb-3'}>
-              <WatchlistBtn/>
+            <div
+              className={
+                "col-md-4 d-flex align-items-center m-p-2 m-justify-content-normal m-mb-3"
+              }
+            >
+              <WatchlistBtn />
             </div>
-            <div className={'col-md-4 ticket-details-holder m-hidden'}>
-              <Ticket ticket={5}/>
+            <div className={"col-md-4 ticket-details-holder m-hidden"}>
+              <Ticket ticket={5} />
             </div>
           </div>
         </div>
       </div>
-      <div className={'mt-6 card-grid'}>
-        <RaffleCard description={'The all new Iphone SE is up for grabsThe all new Iphone SE is up for grabs'}
-                    timer={'1 hr : 30 mins : 27 sec'}
-                    charity
-                    ticket={10}
-                    title={'Tag Heuer Formula 1'}
-                    imgUrl={'https://pngimg.com/uploads/iphone_12/iphone_12_PNG19.png'}/>
-        <RaffleCard description={'The all new Iphone SE is up for grabsThe all new Iphone SE is up for grabs'}
-                    timer={'1 hr : 30 mins : 27 sec'}
-                    title={'Tag Heuer Formula 1'}
-                    imgUrl={'https://pngimg.com/uploads/iphone_12/iphone_12_PNG19.png'}/>
-        <RaffleCard description={'The all new Iphone SE is up for grabsThe all new Iphone SE is up for grabs'}
-                    timer={'1 hr : 30 mins : 27 sec'}
-                    title={'Tag Heuer Formula 1'}
-                    imgUrl={'https://pngimg.com/uploads/iphone_12/iphone_12_PNG19.png'}/>
-        <RaffleCard description={'The all new Iphone SE is up for grabsThe all new Iphone SE is up for grabs'}
-                    timer={'1 hr : 30 mins : 27 sec'}
-                    charity
-                    ticket={1000}
-                    title={'Tag Heuer Formula 1'}
-                    imgUrl={'https://pngimg.com/uploads/iphone_12/iphone_12_PNG19.png'}/>
-        <RaffleCard
-          description={'The all new Iphone SE is up for grabsThe all new Iphone SE is up for grabs'}
-          timer={'1 hr : 30 mins : 27 sec'}
-          title={'Tag Heuer Formula 1'}
-          imgUrl={'https://pngimg.com/uploads/iphone_12/iphone_12_PNG19.png'}/>
-        <RaffleCard
-          description={'The all new Iphone SE is up for grabsThe all new Iphone SE is up for grabs'}
-          timer={'1 hr : 30 mins : 27 sec'}
-          title={'Tag Heuer Formula 1'}
-          imgUrl={'https://pngimg.com/uploads/iphone_12/iphone_12_PNG19.png'}/>
-        <RaffleCard description={'The all new Iphone SE is up for grabsThe all new Iphone SE is up for grabs'}
-                    timer={'1 hr : 30 mins : 27 sec'}
-                    title={'Tag Heuer Formula 1'}
-                    imgUrl={'https://pngimg.com/uploads/iphone_12/iphone_12_PNG19.png'}/>
-      
-      
+      <div className={"mt-6 mb-5 card-grid"}>
+        {raffles &&
+          raffles.map((raffle) => (
+            <RaffleCard
+              key={raffle.id}
+              description={raffle.description}
+              timer={raffle.start_time}
+              charity={raffle.charity}
+              ticket={raffle.number_of_tickets}
+              title={raffle.name}
+              imgUrl={raffle.image_url}
+              // imgUrl={"https://pngimg.com/uploads/iphone_12/iphone_12_PNG19.png"}
+            />
+          ))}
       </div>
-      <div className={'mt-6 m-background'}>
-        <div className={'m-flex justify-content-between'}>
-          <div className={'col-md-10'}>
-            <div className={'d-flex align-items-center'}>
+      <div className={"mt-2 m-background"}>
+        <div className={"m-flex justify-content-between"}>
+          <div className={"col-md-10"}>
+            <div className={"d-flex align-items-center"}>
               <div className={"title2"}>Elivate Prize Winners</div>
-              <img alt={'icon'} src={require('../../assets/icons/prize.svg')}/>
+              <img alt={"icon"} src={require("../../assets/icons/prize.svg")} />
             </div>
-            <div className={'paragraph'}>
+            <div className={"paragraph"}>
               Raffles recently won, don't miss out on the next raffles!
             </div>
           </div>
-          <div className={'col-md-2'}>
-            <FormButton title={'View all winners'}/>
+          <div className={"col-md-2"}>
+            <FormButton title={"View all winners"} />
           </div>
         </div>
-        
-        
-        <div className={"col-md-12 winners-holder-div justify-content-center m-flex m-align-items-center"}>
-          <HomepageWinners tickets={5}/>
-          <HomepageWinners tickets={4}/>
-          <HomepageWinners tickets={5}/>
+
+        <div
+          className={
+            "col-md-12 winners-holder-div justify-content-center m-flex m-align-items-center"
+          }
+        >
+          <HomepageWinners tickets={5} />
+          <HomepageWinners tickets={4} />
+          <HomepageWinners tickets={5} />
+          <HomepageWinners tickets={3} />
         </div>
       </div>
-      <Testimonials/>
+      <Testimonials />
     </div>
   );
-}
+};
 export default Home;
---
+++ src/pages/home/components/FeaturedRaffleTitle.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { SubHeader } from "./Subheader";
+
+export const FeaturedRaffleTitle = () => {
+  return (
+    <div className={"text-align-center mt-6 container"}>
+      <SubHeader
+        subheader={"Featured Raffle"}
+        text={"Bidding on Elivate9ja happens in THREE easy steps"}
+      />
+    </div>
+  );
+};
