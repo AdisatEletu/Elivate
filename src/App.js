a31d99beb814e23530630a6f3e8f0a8e2dc4be48
Adisat Eletu <adisat@softcom.ng>
2021-10-09 16:39:49 +0100

    base url update

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export const initialize = async ( axiosLib) => {
   if (window.location.href.includes("elivate9jaonline")) {
     axios.defaults.baseURL = " https://api.elivate9jaonline.com/api";
   } else{
-    axiosLib.defaults.baseURL = "https://desolate-fjord-54053.herokuapp.com/api/";
+    axiosLib.defaults.baseURL = "https://desolate-fjord-54053.herokuapp.com/api";
   } 
   
   axios.defaults.headers.common[
